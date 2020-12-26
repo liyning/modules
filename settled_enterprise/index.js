24b90d2b6f22f25c951a9dbb3b0076e1983fd4ea
liyning <1772671980@qq.com>
2020-12-26 21:35:55 +0800

    mod npm start script

--- settled_enterprise/index.js
+++ settled_enterprise/index.js
@@ -2,7 +2,7 @@ import CopyWebpackPlugin=require('copy-webpack-plugin');
 new CopyWebpackPlugin([
             {
                 from: __dirname+'/settled_enterprise',
-                to: __dirname+'/pages/modules/',
+                to: __dirname+'/pages/modules/platform_app/',
                 ignore: ['.*']
             }
         ])

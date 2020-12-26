ce720b599e575f630780144499b648d93bdb9781
liyning <1772671980@qq.com>
2020-12-27 02:18:06 +0800

    add copy

--- settled_enterprise/index.js
+++
@@ -1,8 +0,0 @@
-import CopyWebpackPlugin=require('copy-webpack-plugin');
-new CopyWebpackPlugin([
-            {
-                from: __dirname+'/settled_enterprise',
-                to: __dirname+'/pages/modules/platform_app/',
-                ignore: ['.*']
-            }
-        ])
